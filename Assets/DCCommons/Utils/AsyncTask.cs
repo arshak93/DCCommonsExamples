4cb71b28fad7338609a55c81890294341d782b12
Arshak Minasyan <arshakminasyan93@gmail.com>
2017-12-08 20:42:17 +0400

    Added AsyncTask with no parameters

--- Assets/DCCommons/Utils/AsyncTask.cs
+++ Assets/DCCommons/Utils/AsyncTask.cs
@@ -1,6 +1,38 @@
 ﻿using System;
 
 namespace DCCommons.Utils {
+	
+	public class AsyncTask {
+
+		public delegate void TaskSuccessDelegate();
+		public delegate void TaskFailDelegate(Exception error);
+
+		private TaskSuccessDelegate onSuccess;
+		private TaskFailDelegate onFail;
+
+		public AsyncTask OnSuccess(TaskSuccessDelegate onSuccess) {
+			this.onSuccess = onSuccess;
+			return this;
+		}
+
+		public AsyncTask OnFail(TaskFailDelegate onFail) {
+			this.onFail = onFail;
+			return this;
+		}
+
+		public void Success() {
+			if (onSuccess != null) {
+				onSuccess();
+			}
+		}
+
+		public void Fail(Exception error) {
+			if (onFail != null) {
+				onFail(error);
+			}
+		}
+	}
+	
 	public class AsyncTask<T> {
 
 		public delegate void TaskSuccessDelegate(T result);
